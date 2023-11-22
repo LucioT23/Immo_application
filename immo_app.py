befbd3b5b00996b2ee460509b16d9788c2342f46
LucioT23 <104279561+LucioT23@users.noreply.github.com>
2023-11-22 14:49:25 +0100

    Add files via upload

--- immo_app.py
+++ immo_app.py
@@ -117,16 +117,18 @@ with col1:
 
 with col2:
     #st.subheader("Prix médian par nuit")
-    fig = px.box(filtered_df, x="Number Room", y='euros', template = "seaborn") #template="gridon"
+    fig = px.box(filtered_df, x="Number Room", y='euros', template = "seaborn", text=filtered_df['euros']) #template="gridon"
     fig.update_layout(yaxis_title="Prix € par nuit", xaxis_title = "Nombre de chambres", title = "Prix médian par nuit")
+    fig.update_traces(marker_color='orange', textangle=0,textposition="inside",textfont_size=12, textfont_color="white")
     st.plotly_chart(fig,use_container_width=True)
 
 with col3:
     #st.subheader("Prix moyen par nuit")
     rooms = filtered_df.groupby(by = "Number Room", as_index = False)['euros'].mean()
     rooms['euros'] = rooms['euros'].round().astype(int)
-    fig = px.bar(rooms, x="Number Room", y='euros', template = "seaborn", title = "Prix moyen par nuit") #template = "plotly_dark"
+    fig = px.bar(rooms, x="Number Room", y='euros', template = "seaborn", title = "Prix moyen par nuit", text=rooms['euros']) #template = "plotly_dark"
     fig.update_layout(yaxis_title="Prix € par nuit", xaxis_title = "Nombre de chambres")
+    fig.update_traces(marker_color='orange', textangle=0,textposition="inside",textfont_size=12, textfont_color="white")
     st.plotly_chart(fig,use_container_width=True)
 
 
@@ -205,9 +207,9 @@ with col1_kpi:
   fig.update_traces(marker_color='orange', textangle=0,textposition="inside",textfont_size=12, textfont_color="white")
   st.plotly_chart(fig,use_container_width=True)
 with col2_kpi:
-  fig = px.bar(merged_df, x="Number Room", y='revenue_potential', text_auto = '.2')
+  fig = px.bar(merged_df, x="Number Room", y='revenue_potential', text=merged_df['revenue_potential'])
   fig.update_layout(yaxis_title="Revenu en €", xaxis_title = "Nombre de chambres", title="Revenue Previsionnel")
-  fig.update_traces(marker_color='orange', textangle=0,textposition="inside",textfont_size=12, textfont_color="white",)
+  fig.update_traces(marker_color='orange', textangle=0,textposition="inside",textfont_size=12, textfont_color="white")
   st.plotly_chart(fig,use_container_width=True)
 
 col1_reservation, col2_reservation, col3_reservation = st.columns((3))
@@ -215,21 +217,24 @@ with col1_reservation:
   #st.subheader("Nombre des biens réservés sur cette période")
   #reservation_rooms_count = filtered_df.groupby(by = "Number Room", as_index = False)['jours reserves'].count()
   #reservation_rooms_count = reservation_rooms_count.rename(columns={'jours reserves': 'logements reservés'})
-  fig3 = px.bar(result_by_room, x="Number Room", y='Nombre reservation', template = "seaborn")
+  fig3 = px.bar(result_by_room, x="Number Room", y='Nombre reservation', template = "seaborn",, text=result_by_room['Nombre reservation'])
   fig3.update_layout(yaxis_title="Nombre de biens réservés", xaxis_title = "Nombre de chambres", title="Nombre des biens réservés sur cette période")
+  fig.update_traces(marker_color='orange', textangle=0,textposition="inside",textfont_size=12, textfont_color="white")
   st.plotly_chart(fig3,use_container_width=True)
 
 with col2_reservation:
   #st.subheader("Nombre des jours réservés sur cette période")
   #fig = px.bar(reservation_rooms, x="Number Room", y='Total jours réservés', template = "seaborn")
-  fig = px.bar(result_by_room, x="Number Room", y='Total jours réservés', template = "seaborn")
+  fig = px.bar(result_by_room, x="Number Room", y='Total jours réservés', template = "seaborn", text=result_by_room['Total jours réservés'])
   fig.update_layout(yaxis_title="Nombre de jours réservés", xaxis_title = "Nombre de chambres", title="Nombre des jours réservés sur cette période")
+  fig.update_traces(marker_color='orange', textangle=0,textposition="inside",textfont_size=12, textfont_color="white")
   st.plotly_chart(fig,use_container_width=True)
 
 with col3_reservation:
   #st.subheader("Moyenne de jours réservés par reservation")
-  fig = px.bar(result_by_room, x="Number Room", y='Moyenne jours réservés par reservation')
+  fig = px.bar(result_by_room, x="Number Room", y='Moyenne jours réservés par reservation', text=result_by_room['Moyenne jours réservés par reservation'])
   fig.update_layout(yaxis_title="Moyenne de jours réservés", xaxis_title = "Nombre de chambres", title="Moyenne de jours réservés par reservation")
+  fig.update_traces(marker_color='orange', textangle=0,textposition="inside",textfont_size=12, textfont_color="white")
   st.plotly_chart(fig,use_container_width=True)
 
 # Filtrer les lignes avec des valeurs non nulles dans les colonnes pertinentes
