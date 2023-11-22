05ecdacce1284a596acc5693a540a777d0bc1192
LucioT23 <104279561+LucioT23@users.noreply.github.com>
2023-11-22 11:19:30 +0100

    Add files via upload

--- immo_app.py
+++ immo_app.py
@@ -186,7 +186,7 @@ reservation_rooms = filtered_df.groupby(by = ["Number Room", 'Number annonce'], 
 # Modifie la colonne 'Nombre reservation' en fonction de la valeur de 'Total jours réservés'
 reservation_rooms['Nombre reservation'] = reservation_rooms.apply(lambda row: 0 if row['Total jours réservés'] == 0 else row['Nombre reservation'], axis=1)
 reservation_rooms['occupancy_rate'] = (reservation_rooms['Total jours réservés']/nombre_de_jours)*100
-occupancy_by_room = reservation_rooms.groupby('Number Room', as_index=False).agg({'occupancy_rate': 'mean'})
+occupancy_by_room = reservation_rooms.groupby('Number Room', as_index=False).['occupancy_rate'].mean()
 result_by_room = reservation_rooms.groupby('Number Room', as_index=False).agg({'Nombre reservation': 'sum','Total jours réservés': 'sum'})
 result_by_room['Moyenne jours réservés par reservation'] = result_by_room['Total jours réservés'] / result_by_room['Nombre reservation']
 
@@ -197,12 +197,13 @@ merged_df = pd.merge(rooms_mean_price, occupancy_by_room, on='Number Room')
 merged_df['revenue_potential'] = merged_df['occupancy_rate'] * merged_df['euros']
 
 st.write(merged_df)
+st.write(occupancy_by_room)
 
 #col1_kpi, col2_kpi = st.columns((2))
 #with col1_kpi:
-fig = px.bar(occupancy_by_room, x="Number Room", y='occupancy_rate') #, template = "seaborn"
-fig.update_layout(yaxis_title="Taux d'occupation", xaxis_title = "Nombre de chambres", title="Taux d'occupation", color='orange')
-st.plotly_chart(fig,use_container_width=True)
+#fig = px.bar(occupancy_by_room, x="Number Room", y='occupancy_rate') #, template = "seaborn"
+#fig.update_layout(yaxis_title="Taux d'occupation", xaxis_title = "Nombre de chambres", title="Taux d'occupation", color='orange')
+#st.plotly_chart(fig,use_container_width=True)
 #with col2_kpi:
 #  fig = px.bar(merged_df, x="Number Room", y='revenue_potential')
 #  fig.update_layout(yaxis_title="Revenu en €", xaxis_title = "Nombre de chambres", title="Revenue Previsionnel", color='orange')
