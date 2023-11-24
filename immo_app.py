30d052a7eec6616ecbcc10878986458a4a35490a
LucioT23 <104279561+LucioT23@users.noreply.github.com>
2023-11-24 15:17:14 +0100

    Add files via upload

--- immo_app.py
+++ immo_app.py
@@ -230,10 +230,13 @@ with col1_reservation:
   #st.subheader("Nombre des biens réservés sur cette période")
   #reservation_rooms_count = filtered_df.groupby(by = "Number Room", as_index = False)['jours reserves'].count()
   #reservation_rooms_count = reservation_rooms_count.rename(columns={'jours reserves': 'logements reservés'})
-  fig3 = px.bar(result_by_room, x="Number Room", y='Nombre reservation', template = "seaborn", text=result_by_room['Nombre reservation'])
+  fig3 = go.Figure(data=[
+    go.Bar(result_by_room, x="Number Room", y='Nombre reservation', template = "seaborn", text=result_by_room['Nombre reservation']),
+    go.Bar(rooms, x="Number Room", y='Title', title = "Repartition des biens en Volume", text =rooms['Title'] ))
+  #fig3 = px.bar(result_by_room, x="Number Room", y='Nombre reservation', template = "seaborn", text=result_by_room['Nombre reservation'])
   fig3.update_layout(yaxis_title="Nombre de biens réservés", xaxis_title = "Nombre de chambres", title="Nombre des biens réservés sur cette période")
-  fig.update_traces(textangle=0,textposition="inside",textfont_size=12, textfont_color="white")
-  fig.add_trace(px.bar(rooms, x="Number Room", y='Title', title = "Repartition des biens en Volume", text =rooms['Title'] ))
+  fig3.update_traces(textangle=0,textposition="inside",textfont_size=12, textfont_color="white")
+  #fig.add_trace(px.bar(rooms, x="Number Room", y='Title', title = "Repartition des biens en Volume", text =rooms['Title'] ))
   st.plotly_chart(fig3,use_container_width=True)
 
 with col2_reservation:
